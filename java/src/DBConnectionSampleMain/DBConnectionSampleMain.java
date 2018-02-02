61eec49c08851f8d33ec125d84bd9f26f7a39539
oyajiclub117 <36089095+oyajiclub117@users.noreply.github.com>
2018-02-03 08:38:42 +0900

    import java
    sample

---
+++ java/src/DBConnectionSampleMain/DBConnectionSampleMain.java
@@ -0,0 +1,33 @@
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectionSampleMain {
+	public static void main(String[] args) {
+		String framework = "embedded";
+		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+		String protocol = "jdbc:derby:";
+		try{
+			//Class.forName(driver).newInstance();
+			System.out.println("Derby DB Driver is loaded");
+			Connection conn = null;
+			conn = DriverManager.getConnection(protocol + "C:\\Users\\oyaji\\Documents\\mydata\\db\\derby\\DerbyTestDB");
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from member");
+			while(rs.next()) {
+				System.out.println(
+					rs.getString(1) +
+					"," + 
+					rs.getString(2)
+					);
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+			System.out.println("fin");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
